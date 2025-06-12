1bbf279d5f2eb1d9d405de7f6e75c1e60c547ff4
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2025-06-12 23:49:35 +0300

    initial Setup Spring

---
+++ tradyzer_backend/src/test/java/org/ahmedhammad/tradyzertemp/TradyzerTempApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ahmedhammad.tradyzertemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradyzerTempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
